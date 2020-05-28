f04aa170a7c3cf1e61d7f726d470695a115f8e8d
Sukhov-Dmitriy <65165901+Sukhov-Dmitriy@users.noreply.github.com>
2020-05-28 19:13:16 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -6,6 +6,7 @@
 using namespace std;
 CRat *CreateData(ifstream &input, std::map<string,CRatFactory*> f);
 CRat1 operator + (const CRat &A, const CRat &B);
+CRat1 operator - (const CRat &A, const CRat &B);
 void test1();
 void test2();
 void test3();
